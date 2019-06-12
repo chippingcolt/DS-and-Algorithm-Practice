f844af8ec5e5722391106fceee8f35bd83441586
Timothy Brumbaugh <timothy.brumbaugh@metalab.csun.edu>
2019-06-12 13:45:37 -0700

    Adding Simple Array methods

--- Data_Structures/Linear_Data_Structures/Arrays/Single_Dimensional/SDArrayDriver.java
+++ Data_Structures/Linear_Data_Structures/Arrays/Single_Dimensional/SDArrayDriver.java
@@ -2,11 +2,42 @@ import java.util.concurrent.TimeUnit;
 
 class SDArrayDriver extends SDArray {
     public static void main(String[] args){
+
+        //Look up by index
         long startTime = System.nanoTime();
-        SDArray example = new SDArray();
-        System.out.println(example.checkIndexOfArray(example.createArray(), 4));
+        SDArray lookupEx = new SDArray();
+        System.out.println(lookupEx.checkIndexOfArray(lookupEx.createArray(), 4));
         long endTime = System.nanoTime();
-        System.out.println("Time elapsed for searching an Array in O(1) : " + example.timeIt(startTime, endTime));
+        System.out.println("Time elapsed for looking up an element an Array in O(1): " + lookupEx.timeIt(startTime, endTime) + "\n");
+        
+
+        //Deletion by index
+        startTime = System.nanoTime();
+        SDArray deletionEx = new SDArray();
+        int[] ex = deletionEx.createArray();
+        System.out.println("Old Array");
+        deletionEx.printArray(ex);
+        System.out.println("\nNew Array");
+        //this function will select the index you want to delete and shift everything over
+        //you can't resize an array so the last numbers will be duplicated
+        deletionEx.printArray(deletionEx.deleteElementOfArray(ex, 0, true));
+        endTime = System.nanoTime();
+        System.out.println("\nTime elapsed to delete an element from an Array in O(N) with shifting: " + deletionEx.timeIt(startTime, endTime) + "\n");
+
+        //Insertion
+        startTime = System.nanoTime();
+        SDArray insertEx = new SDArray();
+        ex = insertEx.createArray();
+        System.out.println("Old Array");
+        insertEx.printArray(ex);
+        System.out.println("\nNew Array");
+        // this function will select the index you want to delete and shift everything
+        // over
+        // you can't resize an array so the last numbers will be duplicated
+        insertEx.printArray(insertEx.insertElementOfArray(ex, 32, 2));
+        endTime = System.nanoTime();
+        System.out.println("\nTime elapsed to insert an element from an Array in O(N) with shifting and copying -- Space Complexity O(N): "
+                + insertEx.timeIt(startTime, endTime) + "\n");
 
     }
 }
